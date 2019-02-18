611ff911a9ab01c86c7b88cbcb628ce2cd533f9b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 17:59:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,14 +10,6 @@ class Student
     @grade=grade
   end
 
-  def save
-    sql = <<-SQL
-  INSERT INTO songs (name, album)
-  VALUES (?, ?)
-  SQL
-  DB[:conn].execute(sql, self.name, self/grade)
-  end
-
   def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
@@ -28,4 +20,13 @@ class Student
         SQL
     DB[:conn].execute(sql)
   end
+
+  def save
+    sql = <<-SQL
+  INSERT INTO songs (name, album)
+  VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql, self.name, self/grade)
+  end
+
 end
