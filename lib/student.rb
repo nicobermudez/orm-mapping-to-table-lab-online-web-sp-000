28b6b38ee7f6a9a6f63175e0a83279b194165f8c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 17:59:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
 
   def save
     sql = <<-SQL
-  INSERT INTO songs (name, album)
+  INSERT INTO students (name, grade)
   VALUES (?, ?)
   SQL
   DB[:conn].execute(sql, self.name, self/grade)
