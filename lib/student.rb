0570151e0e7b27eb8ebac072f1bc8a9b532e624c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 17:55:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
-class Student
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+class Students
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name:, grade:, id: nil)
+    @name=name
+    @grade=grade
+    @id=id
+  end
+
+  def self.save()
+  end
 end
